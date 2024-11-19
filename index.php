38886685ea978492450ae4289086d83720b219c7
Tanawat121 <balltheph12@gmail.com>
2024-11-19 11:53:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,24 +6,48 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>การใช้คำสั่งแบบ Condition (กลุ่มคำสั่ง if)</h1>
+<h1>การใช้คำสั่งแบบ Condition (กลุ่มคำสั่ง if)</h1>
     <?php
-        echo "<h2>คำสั่ง if </h2><br><br><br>";
-        $x = 5;
-        $y = 8;
-        echo "\$x = $x <br>";
-        echo "\$y = $y <br>";
-        if($x<$y){
-            echo "คำในตัวแปร x น้อยกว่าค่าในตัวแปร y<br>";
-        }
-        else{
-            echo "ค่าในตัวแปร y น้อยกว่าค่าในตัวแปร x<br>";
-        }
-        echo "................................";
-        echo "<h2> Short hand if</h2>";
-        if ($score>)=50 ? "ได้เกรด P", "ได้เกรด F";
-        echo "<br>";
-        echo "ผลการเรียนของคุณ".$result;
-        echo "................................";
+    echo "<h2?>คำสั่ง if<h2>";
+    $x = 5;
+    $y = 8;
+    echo "\$x = $x <br>";
+    echo "\$y = $y <br>";
+    if($x<$y){
+        echo "ค่าในตัวแปร x น้อยกว่าค่าในตัวแปร y<br>";
+    }
+    else{
+        echo "ค่าในตัวแปร y น้อยกว่าค่าในตัวแปร x<br>";
+    }
+    echo "-------------------------------------";
+    echo "<h2> Short hand if</h2>";
+    $score = 50; 
+    if ($score >= 50) echo "ขอแสดงความยินดีคุณสอบผ่าน";
+    echo "<br>";
+    $result = $score>=50 ? "ได้เกรด P":"ได้เกรด F";
+    echo "<br>";
+    echo "ผลการเรียนของคุณ".$result;
+    echo "<br>";
+    echo "-------------------------------------";
+    echo "<h2>คำสั่ง Switch Case</h2>";
+    echo "<br>";
+    $pet = 'cat';
+    switch ($pet) {
+        case 'Cat':
+            echo "คุณชอบนอนเหมือนแมว<br>";
+            break;
+        case 'Dog':
+            echo "คุณเห่าเหมือนหมา<br>";
+            break;
+        case 'Bird':
+            echo "คุณพูดมากเหมือนนก<br>";
+            break;
+            default:
+            echo "คุณเป็นมนุษย์ Introvert<br>";
+            break;
+    }
+    echo "<br>";
+    echo "-------------------------------";
+    ?>  
 </body>
 </html>
