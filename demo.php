b5d58861787110a394123d377d1324c3e9d4cb0c
Tanawat121 <balltheph12@gmail.com>
2024-11-26 22:50:57 +0700

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ทดสอบส่งคำโดยใช้ method GET</title>
+</head>
+<body>
+    <h2>ทดสอบส่งคำเป็น method GET (ผ่าน URL)</h2>
+    <a href="test_get.php?subject=PHP&web=www.google.com">Clck</a>
+</body>
+</html>
